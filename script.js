7298bf72eac59c7ceefcaa48ca6712bd0eba5f6b
Meece <meece.work@gmail.com>
2022-10-04 18:17:31 +0200

    Added repos fetch request and updates the page with latest repos

--- script.js
+++ script.js
@@ -9,12 +9,23 @@ async function getUser(username) {
         const { data } = await axios(APIURL + username);
 
         createUserCard(data);
+        getRepos(username);
     } catch(err) {
         if (err.response.status == 404) {
             createErrorCard("No Profile Found");
         } else {
             console.log(err);
         }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created');
+
+        addReposToCard(data);
+    } catch(err) {
+        createErrorCard("Problem Fetching Repos");
     }
 }
 
@@ -52,6 +63,20 @@ function createErrorCard(msg) {
     `
 
     main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos.slice(0, 10).forEach(repo => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.textContent = repo.name;
+
+        reposEl.appendChild(repoEl);
+    })
 }
 
 form.addEventListener('submit', (e) => {
